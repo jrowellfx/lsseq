c8a7f7899adb553a854bc3018ac9e81666b0d5e3
root <root@neroli.cgtown>
2012-03-19 10:42:51 -0700

    setup.py distutil instead of cheezey install hack script I had.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='seqLister',
+      version='1.0',
+      description='Number sequence expander and compresser',
+      author='James Philip Rowell',
+      author_email='james@oic-inc.net',
+      url='http://www.orangeimagination.com/',
+      py_modules=['seqLister'],
+     )
