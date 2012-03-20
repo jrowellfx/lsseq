bd01be2b868f79519e08539dcf7529f90772fed9
James Rowell <jrowell@neroli.cgtown>
2012-03-19 22:25:31 -0700

    Up to date license and minor tweeks to setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,12 @@ from distutils.core import setup
 
 setup(name='lsseq',
       version='1.5',
-      description='very ls-like image sequence listing command with supporting module',
+      description='ls-like command for image frame sequences',
+      long_description='lsseq is built to behave very much like ls, but lists image sequences in a compressed manner.  A supporting module is included.',
       author='James Philip Rowell',
       author_email='james@oic-inc.net',
       url='http://www.orangeimagination.com/',
       py_modules=['seqLister'],
       scripts=['lsseq'],
+      license = "BSD 3-Clause license",
      )
