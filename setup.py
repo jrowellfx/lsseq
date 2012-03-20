b2eedca5c874d34e072ee4e5a220b62aeb714ad4
James Rowell <jrowell@neroli.cgtown>
2012-03-19 17:34:12 -0700

    Added setup.py and fixed ddir testfiles to be non-zero length.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='lsseq',
+      version='1.5',
+      description='very ls-like image sequence listing command',
+      author='James Philip Rowell',
+      author_email='james@oic-inc.net',
+      url='http://www.orangeimagination.com/',
+      scripts=['lsseq'],
+     )
