bf3d604615fae59251e20cb4c6b110823d155b13
James Rowell <jrowell@neroli.cgtown>
2012-03-19 21:43:55 -0700

    Minor wording change in comment.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(name='lsseq',
       version='1.5',
-      description='very ls-like image sequence listing command and supporting module',
+      description='very ls-like image sequence listing command with supporting module',
       author='James Philip Rowell',
       author_email='james@oic-inc.net',
       url='http://www.orangeimagination.com/',
