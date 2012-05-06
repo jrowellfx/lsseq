efd101faacc05d1c98ea055d393974726aa2908f
root <root@discreet.cgtown>
2012-05-06 16:23:11 -0700

    Modified setup.py to install new scripts, also added symlink for condenseseq to git

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from distutils.core import setup
 
 setup(name='lsseq',
-      version='1.6',
+      version='1.601',
       description='ls-like command for image frame sequences',
       long_description='lsseq is built to behave very much like ls, but lists image sequences in a condensed manner.  A supporting module is included.',
       author='James Philip Rowell',
       author_email='james@oic-inc.net',
       url='http://www.orangeimagination.com/',
       py_modules=['seqLister'],
-      scripts=['lsseq'],
+      scripts=['lsseq', 'expandseq', 'condenseseq'],
       license = "BSD 3-Clause license",
      )
