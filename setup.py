4d396dabd321b3c08a2366861b2eb1fad448305f
James Rowell <jrowell@discreet.cgtown>
2012-05-06 19:42:17 -0700

    Cleaning up commit, version numbers, adding extra files etc.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='lsseq',
-      version='1.601',
+      version='1.602',
       description='ls-like command for image frame sequences',
       long_description='lsseq is built to behave very much like ls, but lists image sequences in a condensed manner.  A supporting module is included.',
       author='James Philip Rowell',
