1feb7b510ee36aa29f304cd13ef56581171b6202
James Rowell <james@oic.inc.net>
2012-05-10 15:34:20 -0700

    Fixing setup.py to copy expandseq to condenseseq in creating the tarball and in installing the packages if condenseseq is not present (since it's really an exact copy of expandseq - the functionality changes based on the name of the program on the command line.)

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import os
+import shutil
+
+createdCondenseseq = False
+if not os.path.exists('condenseseq') :
+    shutil.copy2('expandseq', 'condenseseq')
+    createdCondenseseq = True
 
 setup(name='lsseq',
-      version='1.602',
+      version='1.700',
       description='ls-like command for image frame sequences',
-      long_description='lsseq is built to behave very much like ls, but lists image sequences in a condensed manner.  A supporting module is included.',
+      long_description='lsseq is built to behave very much like ls, but lists image sequences in a condensed manner.  A supporting module is included and two other useful command line utilities: expandseq and condenseseq.',
       author='James Philip Rowell',
       author_email='james@oic-inc.net',
       url='http://www.orangeimagination.com/',
@@ -13,3 +20,6 @@ setup(name='lsseq',
       scripts=['lsseq', 'expandseq', 'condenseseq'],
       license = "BSD 3-Clause license",
      )
+
+if createdCondenseseq :
+    os.remove('condenseseq')
