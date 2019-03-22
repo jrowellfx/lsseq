e8b21bbb9860d76b925fa2141e7a29632dfe6087
James Philip Rowell <james.rowell@yahoo.com>
2019-03-22 14:53:19 -0500

    fixing copyright end date

--- rollingAvg.py
+++ rollingAvg.py
@@ -2,7 +2,7 @@
 
 # BSD 3-Clause License
 # 
-# Copyright (c) 2008-2018, James Philip Rowell,
+# Copyright (c) 2008-2019, James Philip Rowell,
 # Alpha Eleven Incorporated
 # www.alpha-eleven.com
 # All rights reserved.
