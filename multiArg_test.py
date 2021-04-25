2910227c7123779119904be323d91246eb4525fa
James Rowell <rowellogic-gh@yahoo.com>
2021-04-24 21:07:35 -0500

    test files example uppdated

--- multiArg_test.py
+++ multiArg_test.py
@@ -1,33 +1,33 @@
-#!/usr/bin/python3
-
 import argparse
 
 def store_const_multiple(const, *destinations) :
-    """Returns an `argparse.Action` class that sets multiple argument
-    destinations (`destinations`) to `const`."""
     class store_const_multiple_action(argparse.Action) :
         def __init__(self, *args, **kwargs) :
-            super(store_const_multiple_action, self).__init__(metavar = None, nargs = 0, const = const, *args, **kwargs)
+            super(store_const_multiple_action, self).__init__( \
+                metavar = None, nargs = 0, const = const, *args, **kwargs)
 
         def __call__(self, parser, namespace, values, option_string = None) :
-            for destination in destinations :
-                setattr(namespace, destination, const)
+            for d in destinations :
+                setattr(namespace, d, const)
 
     return store_const_multiple_action
 
 def store_true_multiple(*destinations) :
-    """Returns an `argparse.Action` class that sets multiple argument
-    destinations (`destinations`) to `True`."""
     return store_const_multiple(True, *destinations)
 
 ap = argparse.ArgumentParser()
 
-ap.add_argument("--x", help = "Set `x`.", \
+ap.add_argument("--x", dest="x", help = "Set x to True.",
     action = "store_true", default = False)
-ap.add_argument("--y", help = "Set `y`.", \
-    action = "store_true", default = False)
+ap.add_argument("--X", dest="x", help = "Set x to False.",
+    action = "store_false")
 
-ap.add_argument("--all", help = "Equivalent to `--x --y`.", \
+ap.add_argument("--y", dest="y", help = "Set y to True.",
+    action = "store_true", default = False)
+ap.add_argument("--Y", dest="y", help = "Set y to False.",
+    action = "store_false")
+
+ap.add_argument("--all", help = "Equivalent to `--x --y`.",
     action = store_true_multiple("x", "y"))
 
 args = ap.parse_args()
