c4cdcc2a5765fa3491812dcbe48909c33bab9b08
James Philip Rowell <rowellogic-gh@yahoo.com>
2023-10-22 13:48:29 -0500

    v3.0.1 missed adding the bugfix for --prepentPathRel to cache and movie sequences, as well as when outputting in a different --format (like nuke, or glob) when using --prepandPathRel. This is a quick correction to that oversight, as well as adding a new test director (rdir) and some more tests to make sure this is working as expected.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name            = 'lsseq',
-    version         = '3.0.1',
+    version         = '3.0.2',
     description='ls-like command for image-sequences',
     long_description=long_description,
     long_description_content_type='text/markdown',
